605b048317f0cef02596d40657ef31b5606b0fca
Ali Ismayilov <ali@ismailov.info>
2014-09-08 23:38:48 -0300

    Refactor Identity.find_for_google_oauth

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -4,31 +4,10 @@ class Identity < ActiveRecord::Base
   validates_uniqueness_of :uid, scope: :provider
 
   def self.find_for_google_oauth(auth)
-    identity = Identity.find_by(provider: auth['provider'], uid: auth['uid'])
-    if identity.nil?
-      registered_user = User.find_by(email: auth['info']['email'])
-      if registered_user.nil?
-        user = User.create!(
-          name: auth['info']['name'],
-          email: auth['info']['email'],
-          password: Devise.friendly_token[0,20],
-        )
-        identity = Identity.create!(
-          provider: auth['provider'],
-          uid: auth['uid'],
-          user: user
-        )
-        user
-      else
-        Identity.create!(
-          provider: auth['provider'],
-          uid: auth['uid'],
-          user: registered_user
-        )
-        registered_user
-      end
-    else
-      identity.user
+    user = User.find_or_create_by(email: auth['info']['email']) do |user|
+      user.name = auth['info']['name']
+      user.password = Devise.friendly_token[0,20]
     end
+    Identity.find_or_create_by(provider: auth['provider'], uid: auth['uid'], user: user).user
   end
 end
