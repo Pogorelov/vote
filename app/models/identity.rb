90ec06264f0549930cfced81234254aedb9be50f
Ali Ismayilov <ali@ismailov.info>
2014-09-08 22:47:30 -0300

    Identity uid validation of uniqueness

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,3 +1,5 @@
 class Identity < ActiveRecord::Base
   belongs_to :user, foreign_key: 'voter_id'
+
+  validates_uniqueness_of :uid, scope: :provider
 end
--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Identity, :type => :model do
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_uniqueness_of(:uid).scoped_to(:provider) }
+  end
 end
